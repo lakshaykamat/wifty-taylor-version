80c39589651f7cb4d5be615b3e71cbc6a3b8653c
Ruchi Singh <ruchisingh.devops@gmail.com>
2024-03-15 20:03:43 +0530

    Update Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,21 @@ const HomePage = (props: Props) => {
     <div className=''>
       <nav className='flex bg-[#D9D9D9] h-20 w-full'>
       </nav>
+
       <div className='flex flex-row w-full p-24'>
         <div className='w-1/2 mt-7 ml-7 font-sans'>
           <h1 className='text-7xl font-extrabold'>Taylor <span className='bg-gradient-to-l from-pink-400 via-pink-500 to-red-400 text-transparent bg-clip-text font-extrabold'> Fandom </span></h1>
           <p className='text-xl'>“People haven't always been there for me but music always has.”</p>
           <p className='text-xl'>-Tyalor Swift</p>
+          <button className='border-2 bg-[#92F4DC] h-10 w-64 outline-none rounded-md mt-3 text-sm font-bold font-sans'>CHECK IN INTO SWIFTY SPACE</button>
         </div>
         <div className='w-1/2 flex items-center justify-center'>
           <img src='image/butterfly.png' className=' h-[360px]'/>
         </div>
       </div>
+
+
+
     </div>
   )
 }
