66338822ca9dff9df0de9c13e5881c341965934d
Ruchi Singh <ruchisingh.devops@gmail.com>
2024-03-16 20:28:23 +0530

    Start leaderboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,11 +39,17 @@ export default function RootLayout({
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
+      <nav className='flex bg-[rgb(217,217,217)] h-16'>
+      </nav>
+        <main className="max-w-6xl mx-auto px-6">
         {children}
+        </main>
+       
         {/* <ThemeProvider
           attribute="class"
           defaultTheme="dark"
